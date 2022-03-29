3ec8bb9ff4b2b142fe036533aaad5e6e6a9778ca
EliasGabriel1 <elias.147682@hotmail.com>
2022-03-29 01:37:27 -0300

    update carrossel

--- src/components/Carrossel/index.js
+++ src/components/Carrossel/index.js
@@ -1,15 +1,31 @@
 import BoxReadOurBlog from "../BoxReadOurBlog";
 import Image1 from "../Imagens/Comida1.jpg";
-import {Carrossel} from "./styled";
+import {Carrossel,Container,ButtonOne,ButtonTwo} from "./styled";
 
 function index(){
+    function scrollright() {
+      document.getElementById('container').scrollLeft += 283;
+    };
+    function scrollLeft() {
+      document.getElementById('container').scrollLeft -= 283;
+    };
+     
     return(
-        <Carrossel>
+        <Container>
+            <ButtonOne onClick={scrollLeft} type="button">{"<"}</ButtonOne>
+        <Carrossel id="container">
+            <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
+            <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
+            <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
+            <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
+            <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
             <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
             <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
             <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
             <BoxReadOurBlog src={Image1} alt="alt" text="Broccoli Salad width Bacon" buttontext="See Recipe"/>
         </Carrossel>
+            <ButtonTwo onClick={scrollright} type="button">{">"}</ButtonTwo>
+        </Container>
     );
 }
 
--- src/components/Carrossel/styled.js
+++ src/components/Carrossel/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { generateMedia } from "styled-media-query";
 
 const customMedia = generateMedia({
@@ -8,13 +8,62 @@ const customMedia = generateMedia({
 });
 
 export const Carrossel = styled.div`
-    display: flex;
-    justify-content: space-between;
-    max-width: 1200px;
-    overflow: auto;
-    padding: 20px 10px;
-    width: 100%;
-    gap: 40px;        
-    ${customMedia.lessThan("mobile")`
+  scroll-behavior: smooth;
+  display: flex;
+  justify-content: space-between;
+  max-width: 1230px;
+  overflow: auto;
+  padding: 20px 10px;
+  width: 100%;
+  gap: 30px;
+  ${customMedia.lessThan("desktop")`
+      &::-webkit-scrollbar-thumb{background-color: blue;}
     `};
+
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #ffffff;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: transparent;
+    border-radius: 20px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 1300px;
+`;
+
+export const ButtonOne = styled.button`
+  width: 30px;
+  height: 30px;
+  border-radius: 20px;
+  background-color: #F1f2f3;
+  display: flex;
+  align-items :center;
+  justify-content: center;
+  font-size: 30px;  
+  cursor: pointer;
+  margin-right: 5px;
+  color: #11111190
+`;
+
+export const ButtonTwo = styled.button`
+  height: 30px;
+  width: 30px;
+  border-radius: 20px;
+  background-color: #F1f2f3;
+  color: #11111190;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 35px;  
+  cursor: pointer;
+  margin-left: 5px;
 `;
